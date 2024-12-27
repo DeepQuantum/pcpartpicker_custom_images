3322b93829184585a4f2321df63143fda807691f
Damian <damixd02@gmail.com>
2024-12-27 07:12:06 +0100

    some persistence

--- background.js
+++ background.js
@@ -1,5 +1,22 @@
 document.body.style.border = "5px solid red";
 
+loadImages = () => {
+    image_anchors = document.querySelectorAll(".tr__product:not(.tr__product--another)>.td__image>a[href='#view_custom_part']");
+    image_anchors.forEach((image_anchor) => {
+        id = image_anchor.getAttribute("onclick").split("'")[1].split("-")[0];
+        image_url = localStorage.getItem(id);
+        if (image_url) {
+            image_anchor.childNodes[0].setAttribute("src", image_url);
+            console.log("added image")
+        }
+        else {
+            console.log("couldnt load image " + image_url)
+        }
+    })
+}
+
+loadImages();
+
 add_image_url_field = document.createElement('li');
 add_image_url_field.innerHTML = "<label class='show'>Custom Image URL: </label><input id='add_custom_image' name='image_url' type='text' maxlength='4096'></input>"
 
@@ -7,7 +24,7 @@ button = document.getElementById("add_custom_submit")
 button.addEventListener("click", () => {
     id = document.getElementById("add_custom_id").getAttribute("value");
     url = document.getElementById("add_custom_image").value;
-    storeImage(id, url);
+    localStorage.setItem(id, url);
 })
 
 dialog = document.getElementById("partlist_upp_dialog")
